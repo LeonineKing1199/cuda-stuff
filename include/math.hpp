64784c56e465ca47675698599d588d989a8ce3d7
LeonineKing1199 <christian.mazakas@gmail.com>
2016-07-30 16:55:17 -0700

    Began linear algebra library

--- include/math.hpp
+++ include/math.hpp
@@ -3,6 +3,10 @@
 
 #include <array>
 #include <type_traits>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <numeric>
 
 #include "common.hpp"
 
@@ -12,16 +16,138 @@ namespace reg
   struct greater_than_three
     : std::integral_constant<bool, (N > 3)>
   {};
+  
+  template <int N>
+  struct greater_than_zero
+    : std::integral_constant<bool, (N > 0)>
+  {};
 }
 
 // N x M matrix of type T
 template <
-  typename T,
-  int N,
-  int M,
+  typename T, int N, int M,
   typename = reg::enable_if_t<std::is_floating_point<T>::value>
 >
 using matrix_t = std::array<T, N * M>;
+
+// compare two matrices for equality
+template <typename T, int N, int M>
+auto operator==(
+  matrix_t<T, N, M> const& a,
+  matrix_t<T, N, M> const& b)
+-> bool
+{
+  for (int i = 0; i < a.size(); ++i)
+    if (a[i] != b[i])
+      return false;
+     
+  return true;
+}
+
+// a vector of N elements of type T
+template <typename T, int N>
+using vector_t = matrix_t<T, 1, N>;
+
+// compute the dot product of two vectors
+template <typename T, int N>
+auto dot(
+  vector_t<T, N> const& a,
+  vector_t<T, N> const& b)
+-> T
+{
+  vector_t<T, N> c = { 0 };
+  
+  std::transform(
+    a.begin(), a.end(), b.begin(), // we iterate over a and b
+    c.begin(),                     // writing to c
+    std::multiplies<T>{});         // c[i] = a[i] * b[i]
+    
+  return std::accumulate(c.begin(), c.end(), 0); // return reduction
+}
+
+// get the i'th row of a matrix as a vector type
+template <typename T, int N, int M>
+auto row(
+  matrix_t<T, N, M> const& a,
+  int const row_idx)
+-> vector_t<T, M>
+{
+  vector_t<T, M> row{ 0 };
+  
+  // for the number of columns...
+  for (int j = 0; j < M; ++j)
+    // iterate the row at row_idx * num_cols
+    row[j] = a[row_idx * M + j];
+  
+  return row;
+}
+
+// get the j'th column of a matrix as a vector type
+template <typename T, int N, int M>
+auto col(
+  matrix_t<T, N, M> const& a,
+  int const col_idx)
+-> vector_t<T, N>
+{
+  vector_t<T, N> col{ 0 };
+  
+  // for the number of rows...
+  for (int i = 0; i < N; ++i)
+    // iterate the column at col_idx
+    col[i] = a[i * M + col_idx];
+  
+  return col;
+}
+
+// (N x M) x (M x P) -> (N x P)
+// product of two matrices
+template <typename T, int N, int M, int P>
+auto matrix_mul(
+  matrix_t<T, N, M> const& a,
+  matrix_t<T, M, P> const& b)
+-> matrix_t<T, N, P>
+{
+  matrix_t<T, N, P> c = { 0 };
+  
+  for (int i = 0; i < N; ++i)
+    for (int j = 0; j < P; ++j)
+      c[i * N + j] = dot<T, M>(row<T, N, M>(a, i), col<T, M, P>(b, j));
+  
+  return c;
+}
+
+
+// Get the P part of PA = LU
+template <typename T, int N>
+auto pemutation_matrix(matrix_t<T, N, N> const& a) -> matrix_t<T, N, N>
+{
+  using mat = matrix_t<T, N, N>;
+  
+  mat p{ 0 };
+  
+  for (int i = 0; i < N; ++i)
+    for (int j = 0; j < N; ++j)
+      p[i * N + j] = (i == j);
+    
+  for (int i = 0; i < N; ++i) {
+    int max_j = i;
+    
+    for (int j = i; j < N; ++j)
+      if (fabs(a[j * N + i]) > fabs(a[max_j * N + i]))
+        max_j = j;
+      
+    if (max_j != i)
+      for (int k = 0; k < N; ++k) {
+        auto tmp = p[i * N + k];
+        p[i * N + k] = p[max_j * N + k];
+        p[max_j * N + k] = tmp;
+      }
+  }
+  
+  return p;
+}
+
+
 
 // 1x1 implementation
 template <typename T>
--- tests/math-tests.cu
+++ tests/math-tests.cu
@@ -31,6 +31,59 @@ auto math_tests(void) -> void
     assert((det<float, 4>(m) == 1337));
   }
   
+  // Make sure we can validly compare two matrices or not
+  {
+    matrix_t<float, 2, 3> a{
+      1, 2, 3,
+      4, 5, 6};
+      
+    auto b = a;
+    
+    assert((a == b));
+    
+    matrix_t<float, 2, 3> c{
+      1, 2, 3,
+      4, 5, 7};
+      
+    assert((a == c) == false);
+  }
+  
+  // Test the dot product
+  {
+    using vector = vector_t<float, 3>;
+    
+    vector a{1, 2, 3};
+    vector b{4, 6, 8};
+    
+    assert((dot<float, 3>(a, b) == 40));
+  }
+  
+  // We should be able to multiply two matrices
+  {
+    matrix_t<float, 2, 3> a{
+      1, 2, 3,
+      4, 5, 6};
+      
+    matrix_t<float, 3, 2> b{
+      7, 8,
+      9, 10,
+      11, 12};
+    
+    vector_t<float, 3> r = row<float, 2, 3>(a, 0);
+    
+    assert((row<float, 2, 3>(a, 0) == vector_t<float, 3>{1, 2, 3}));
+    assert((row<float, 2, 3>(a, 1) == vector_t<float, 3>{4, 5, 6}));
+      
+    assert((col<float, 3, 2>(b, 0) == vector_t<float, 3>{7, 9, 11}));
+    assert((col<float, 3, 2>(b, 1) == vector_t<float, 3>{8, 10, 12}));
+      
+    auto product = matrix_mul<float, 2, 3, 2>(a, b);
+        
+    assert((product == matrix_t<float, 2, 2>{
+      58, 64,
+      139, 154}));
+  }
+  
   std::cout << "Completed math tests!\n" << std::endl;
 }
 
