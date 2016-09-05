c3d4f1482136b5baf99c58a98302a9146b3d55c7
LeonineKing1199 <christian.mazakas@gmail.com>
2016-09-05 14:23:47 -0700

    Got point redistribution prototype to work!

--- include/lib/redistribute-pts.hpp
+++ include/lib/redistribute-pts.hpp
@@ -1,6 +1,8 @@
 #ifndef REGULUS_LIB_REDISTRIBUTE_PTS_HPP_
 #define REGULUS_LIB_REDISTRIBUTE_PTS_HPP_
 
+#include <stdio.h>
+
 #include "../globals.hpp"
 #include "../math/tetra.hpp"
 #include "../math/point.hpp"
@@ -10,6 +12,7 @@ template <typename T>
 __global__
 void redistribute_pts(
   int const assoc_size,
+  int const num_tetra,
   tetra const* __restrict__ mesh,
   point_t<T> const* __restrict__ pts,
   int const* __restrict__ nm,
@@ -30,25 +33,29 @@ void redistribute_pts(
     // and we want to test validity of nm_ta[ta[tid]] because we're going
     // in reverse
     int const pa_tid = nm_ta[ta_id];
-    
+
     // this means the tetrahedron was not even written to
-    if (pa_tid != -1) {
+    if (pa_tid == -1) {
       return;
     }
-    
+
     // this point was not ultimately nominated even though it wrote
     // this check my ultimately be unnecessary but for now I'm going
     // to be safe (I think it's necessary)
     if (nm[pa_tid] != 1) {
       return;
     }
-    
+
+    if (pa_tid == tid) {
+      return;
+    }
+
     // we now know that pa_tid is actually a valid tuple id!
     // invalidate this association
     ta[tid] = -1;
     pa[tid] = -1;
     la[tid] = -1;
-    
+
     // we know that we wrote to mesh at ta_id and that we then wrote
     // past the end of the mesh at fl[tid] + { 0[, 1[, 2]] }
     array<int, 4> local_pa{-1};
@@ -59,43 +66,59 @@ void redistribute_pts(
     
     array<tetra, 4> tets;
     int tet_size = 0;
-    
+
     // load the tetrahedra onto the stack
+    // I really wanna create a stack-based vector
+    // to use for this though
     tets[tet_size] = mesh[ta_id];
     local_ta[tet_size] = ta_id;
     local_pa[tet_size] = pa_id;
     ++tet_size;
     
     int const fract_size = __popc(la[pa_tid]);
-    int const mesh_offset = fl[pa_tid];
+    int const mesh_offset = num_tetra + fl[pa_tid];
+    
+    /*printf("pa_tid: %d, pa: %d, ta: %d, la: %d\n \
+fract_size: %d; mesh_offset: %d\n",
+           pa_tid, pa[pa_tid], ta[pa_tid], la[pa_tid],
+           fract_size, mesh_offset);*/
+    
+    
     for (int i = 0; i < (fract_size - 1); ++i) {
       tets[tet_size] = mesh[mesh_offset + i];
       local_ta[tet_size] = mesh_offset + i;
       local_pa[tet_size] = pa_id;
       ++tet_size;
     }
-    
+
     // now we can begin testing each one
+    point_t<T> const p = pts[pa_id];
     for (int i = 0; i < tet_size; ++i) {
-      tetra const t{tets[i]};
+      tetra const t = tets[i];
       
-      point_t<T> const a{pts[t.x]};
-      point_t<T> const b{pts[t.y]};
-      point_t<T> const c{pts[t.z]};
-      point_t<T> const d{pts[t.w]};
-      
-      point_t<T> const p{pts[pa_id]};
+      point_t<T> const a = pts[t.x];
+      point_t<T> const b = pts[t.y];
+      point_t<T> const c = pts[t.z];
+      point_t<T> const d = pts[t.w];
       
       local_la[la_size] = loc<T>(a, b, c, d, p);
       ++la_size;
     }
-    
+
     // now we have to do a write-back to main memory
-    int const assoc_offset = 4 * atomicAdd(num_redistributions, 1);
+    int const assoc_offset = assoc_size + (4 * atomicAdd(num_redistributions, 1));
     
-    *reinterpret_cast<int4*>(pa) = *reinterpret_cast<int4*>(local_pa.begin());
-    *reinterpret_cast<int4*>(ta) = *reinterpret_cast<int4*>(local_ta.begin());
-    *reinterpret_cast<int4*>(la) = *reinterpret_cast<int4*>(local_la.begin());
+    //printf("assoc_offset: %d\n", assoc_offset);
+    
+    for (int i = 0; i < 4; ++i) {
+      pa[assoc_offset + i] = local_pa[i];
+      ta[assoc_offset + i] = local_ta[i];
+      la[assoc_offset + i] = local_la[i];
+    }
+    
+    /**(reinterpret_cast<int4*>(pa + assoc_offset) = *reinterpret_cast<int4*>(local_pa.begin());
+    *(reinterpret_cast<int4*>(ta + assoc_offset) = *reinterpret_cast<int4*>(local_ta.begin());
+    *(reinterpret_cast<int4*>(la + assoc_offset) = *reinterpret_cast<int4*>(local_la.begin());*/
   }
 }
 
--- tests/redistribute-pts-tests.cu
+++ tests/redistribute-pts-tests.cu
@@ -1,9 +1,193 @@
+#include <thrust/device_vector.h>
+
 #include "test-suite.hpp"
+
+#include "../include/globals.hpp"
+#include "../include/domain.hpp"
+
+#include "../include/lib/calc-ta-and-pa.hpp"
+#include "../include/lib/nominate.hpp"
+#include "../include/lib/fract-locations.hpp"
+#include "../include/lib/fracture.hpp"
 #include "../include/lib/redistribute-pts.hpp"
+
+template <typename T>
+__global__
+void assert_givens(
+  int const assoc_size,
+  int const* __restrict__ pa,
+  int const* __restrict__ ta,
+  point_t<T>* const __restrict__ pts,
+  tetra const* __restrict__ mesh)
+{
+  for (auto tid = get_tid(); tid < assoc_size; tid += grid_stride()) {
+    point_t<T> const p = pts[pa[tid]];
+    tetra const t = mesh[ta[tid]];
+    
+    point_t<T> const a = pts[t.x];
+    point_t<T> const b = pts[t.y];
+    point_t<T> const c = pts[t.z];
+    point_t<T> const d = pts[t.w];
+    
+    assert(loc<T>(a, b, c, d, p) != -1);
+  }
+}
 
 auto redistribute_pts_tests(void) -> void
 {
   std::cout << "Beginning redistribution tests!" << std::endl;
+  
+  // We should be able to redistribute points to the a new
+  // set of tetrahedra from a fracture
+  {
+    using real = float;
+    
+    // generate cartesian grid points
+    int const grid_length = 9;
+    int const root_coord_val = 3 * grid_length;
+    thrust::device_vector<point_t<real>> pts;
+    pts.reserve(grid_length * grid_length * grid_length + 4);
+    pts = gen_cartesian_domain<real>(grid_length);
+
+    int const num_pts = pts.size();
+    assert(num_pts == (grid_length * grid_length * grid_length));
+    
+    int const num_est_tetrahedra = 8 * num_pts;
+
+    // create the points for the initial global tetrahedron,
+    // adding them to the memory buffer
+    point_t<real> const root_a{0, 0, 0};
+    point_t<real> const root_b{root_coord_val, 0, 0};
+    point_t<real> const root_c{0, root_coord_val, 0};
+    point_t<real> const root_d{0, 0, root_coord_val};
+    
+    pts.push_back(root_a);
+    pts.push_back(root_b);
+    pts.push_back(root_c);
+    pts.push_back(root_d);
+    
+    // create root tetrahedron
+    int const pts_size{(int ) pts.size()};
+    int const a{pts_size - 4};
+    int const b{pts_size - 3};
+    int const c{pts_size - 2};
+    int const d{pts_size - 1};
+    tetra const t{a, b, c, d};
+    
+    // initialize mesh
+    thrust::device_vector<tetra> mesh;
+    mesh.reserve(num_est_tetrahedra);
+    mesh[0] = t;
+    
+    // initialize the association arrays
+    thrust::device_vector<int> pa{num_est_tetrahedra, -1};
+    thrust::device_vector<int> ta{num_est_tetrahedra, -1};
+    thrust::device_vector<int> la{num_est_tetrahedra, -1};
+    thrust::device_vector<int> fl{num_est_tetrahedra, -1};
+    
+    assert(pa.capacity() == 8 * (grid_length * grid_length * grid_length));
+    
+    calc_initial_assoc<real><<<bpg, tpb>>>(
+      pts.data().get(),
+      num_pts,
+      t,
+      pa.data().get(),
+      ta.data().get(),
+      la.data().get());
+    
+    cudaDeviceSynchronize();
+    
+    int const assoc_size = num_pts;
+
+    for (int i = 0; i < assoc_size; ++i) {
+      assert(ta[i] == 0);
+    }
+
+    assert_givens<real><<<bpg, tpb>>>(
+      assoc_size,
+      pa.data().get(),
+      ta.data().get(),
+      pts.data().get(),
+      mesh.data().get());
+    
+    cudaDeviceSynchronize();
+    
+    int const num_tetra{1};
+    
+    thrust::device_vector<int> nm{num_pts, 1};
+    thrust::device_vector<int> nm_ta{num_tetra, -1};
+    
+    nominate<<<bpg, tpb>>>(
+      assoc_size,
+      ta.data().get(),
+      pa.data().get(),
+      nm_ta.data().get(),
+      nm.data().get());
+    
+    repair_nm_ta<<<bpg, tpb>>>(
+      assoc_size,
+      pa.data().get(),
+      ta.data().get(),
+      nm.data().get(),
+      nm_ta.data().get());
+    
+    cudaDeviceSynchronize();
+
+    assert(nm[pa[nm_ta[0]]] == 1);
+
+    thrust::device_vector<int> num_redistributions{1, 0};
+    
+    fract_locations(
+      pa.data().get(),
+      nm.data().get(),
+      la.data().get(),
+      assoc_size,
+      fl.data().get());
+        
+    fracture<<<bpg, tpb>>>(
+      assoc_size,
+      num_tetra,
+      pa.data().get(),
+      ta.data().get(),
+      la.data().get(),
+      nm.data().get(),
+      fl.data().get(),
+      mesh.data().get());
+    
+    redistribute_pts<real><<<bpg, tpb>>>(
+      assoc_size,
+      num_tetra,
+      mesh.data().get(),
+      pts.data().get(),
+      nm.data().get(),
+      nm_ta.data().get(),
+      fl.data().get(),
+      pa.data().get(),
+      ta.data().get(),
+      la.data().get(),
+      num_redistributions.data().get());
+    
+    cudaDeviceSynchronize();
+    
+    assert(num_redistributions[0] > 0);
+    
+    auto begin = thrust::make_zip_iterator(
+      thrust::make_tuple(pa.begin(), ta.begin(), la.begin()));
+    
+    thrust::sort(begin, begin + pa.size(), [] __device__ (
+      thrust::tuple<int, int, int> const&a,
+      thrust::tuple<int, int, int> const&b) -> bool
+    {      
+      unsigned int const pa_a = reinterpret_cast<unsigned int const&>(thrust::get<0>(a));
+      unsigned int const pa_b = reinterpret_cast<unsigned int const&>(thrust::get<0>(b));
+      
+      return pa_a < pa_b;
+    });
+    
+    for (int i = 0; i < pa.size(); ++i) {
+      std::cout << pa[i] << " " << ta[i] << " " << la[i] << std::endl;
+    }
+  }
   
   std::cout << "All tests passed!\n" << std::endl;
 }
