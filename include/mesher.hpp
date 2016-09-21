30b507b48a45fb0b4111d0d37964a8c73cbe78a5
LeonineKing1199 <christian.mazakas@gmail.com>
2016-09-20 20:42:37 -0700

    Refactored meshing code

--- include/mesher.hpp
+++ include/mesher.hpp
@@ -23,6 +23,23 @@
 #include "lib/redistribute-pts.hpp"
 #include "lib/get-assoc-size.hpp"
 
+using thrust::host_vector;
+using thrust::device_vector;
+using thrust::make_zip_iterator;
+using thrust::make_transform_iterator;
+using thrust::make_tuple;
+using thrust::copy;
+using thrust::sort_by_key;
+using thrust::device_ptr;
+using thrust::tuple;
+using thrust::get;
+using thrust::remove_if;
+using thrust::distance;
+using thrust::fill;
+
+
+// Okay, so bugs do exist and maybe asserts do need to be integrated
+// into the project at the moment T_T
 __global__
 void assert_unique_mesher(
   int const assoc_size,
@@ -108,6 +125,13 @@ void assert_mesher_associations(
   }
 }
 
+
+
+
+
+
+
+// Main interface for the entire project
 template <typename T>
 class mesher
 {
@@ -116,187 +140,162 @@ private:
   // of points.
   // Each tetra structure contains 4 indices, each one
   // corresponding to a point in the pts_ array
-  point_t<T>* pts_;
-  tetra* tets_;
+  device_vector<point_t<T>> pts_;
+  tetra* tetra_;
   
   int num_pts_;
-  int num_tets_;
-  
-  // pa = point association
-  // ta = tetrahedral association
-  // la = location association
-  //
-  // pa[i] means point p at pts_[i] is
-  // in the tetrahedron tets_[ta[i]] with
-  // a location association of la[i]
-  int* pa_;
-  int* ta_;
-  int* la_;
-    
-  int assoc_size_;
-  int assoc_capacity_;
+  int num_tetra_;
   
   // sort the internal point set by its peanokey
   auto sort_by_peanokey(void) -> void
   {
-    auto const keys_begin = thrust::make_transform_iterator(
-      thrust::device_ptr<point_t<T>>(pts_), peanokey_hash<T>{});
+    auto const keys_begin = make_transform_iterator(
+      pts_.begin(), peanokey_hash<T>{});
     
-    thrust::device_vector<peanokey> keys{keys_begin, keys_begin + num_pts_};
-    thrust::sort_by_key(
-      thrust::device,
-      keys.begin(), keys.end(),
-      pts_);
+    device_vector<peanokey> keys{keys_begin, keys_begin + num_pts_};
+    
+    sort_by_key(keys.begin(), keys.end(), pts_.begin());
   }
-  
-  // we construct our point set with a user-supplied one and sort it
-  // automatically by its peanokey
-  auto init_pts(thrust::host_vector<point_t<T>> const& pts) -> void
-  {
-    num_pts_ = pts.size() - 4;
     
-    cudaMalloc(&pts_, pts.capacity() * sizeof(*pts_));
-    
-    thrust::copy(
-      thrust::device,
-      pts.begin(), pts.end(),
-      pts_);
-                
-    sort_by_peanokey();
-  }
-  
   // assume num_pts_ has been constructed
   auto init_tets(tetra const t) -> void
   {
     int const est_num_tetra = 8 * num_pts_;
-    cudaMalloc(&tets_, est_num_tetra * sizeof(*tets_));
-    *thrust::device_ptr<tetra>(tets_) = t;
-    num_tets_ = 1;
+    cudaMalloc(&tetra_, est_num_tetra * sizeof(*tetra_));
+    *thrust::device_ptr<tetra>(tetra_) = t;
+    num_tetra_ = 1;
   }
-  
-  auto init_assoc(tetra const t) -> void
-  {
-    // eh, on average 8 associations per point seems reasonable
-    // for most cases
-    int const est_num_associations = 8 * num_pts_;
-    int const bytes = est_num_associations * sizeof(int);
     
-    cudaMalloc(&la_, bytes);
-    cudaMalloc(&ta_, bytes);
-    cudaMalloc(&pa_, bytes);
-    
-    auto begin = thrust::make_zip_iterator(thrust::make_tuple(pa_, ta_, la_));
-    
-    thrust::fill(
-      thrust::device,
-      begin, begin + est_num_associations,
-      thrust::make_tuple(-1, -1, -1));
-    
-    calc_initial_assoc<T><<<bpg, tpb>>>(
-      pts_, num_pts_,
-      t,
-      pa_, ta_, la_);
-    
-    cudaDeviceSynchronize();
-    
-    assoc_size_ = num_pts_;
-    assoc_capacity_ = est_num_associations;
-  }
-  
 public:
   // am I asking a constructor to do too much here?
   // is Thrust-only support too exclusive?
   mesher(
-    thrust::host_vector<point_t<T>> const& h_pts,
+    host_vector<point_t<T>> const& h_pts,
     tetra const& root_tet)
+  : pts_{h_pts}
   {
-    init_pts(h_pts);
+    // the last 4 points are the root tetrahedron
+    num_pts_ = pts_.size() - 4;
+    sort_by_peanokey();
+    
     init_tets(root_tet);
-    init_assoc(root_tet);
   }
   
   ~mesher(void)
   {
-    cudaFree(pts_);
-    cudaFree(tets_);
-    cudaFree(pa_);
-    cudaFree(ta_);
-    cudaFree(la_);
+    cudaFree(tetra_);
   }
     
   auto triangulate(void) -> void
   {
+    // allocate storage for the association buffers...
+    int const assoc_capacity = 8 * num_pts_;
+    device_vector<int> pa{assoc_capacity, -1};
+    device_vector<int> ta{assoc_capacity, -1};
+    device_vector<int> la{assoc_capacity, -1};
+    
+    // then build the initial associations
+    {
+      tetra const t = *device_ptr<tetra>(tetra_);
+    
+      calc_initial_assoc<T><<<bpg, tpb>>>(
+        pts_.data().get(),
+        num_pts_, t,
+        pa.data().get(),
+        ta.data().get(),
+        la.data().get());
+      
+      cudaDeviceSynchronize();  
+    }
+        
+    int assoc_size{num_pts_};
+    
     // need to select points
     // need to insert points
     // need to redistribute points
     // need to test points for delauanyhood
     // need to refine
     // need to redistribute points
-    while (assoc_size_ != 0) {
+    while (assoc_size != 0) {
       std::cout << "Allocating temporary buffers..." << std::endl;
       
-      thrust::device_vector<int> nm{num_pts_, 1};
-      thrust::device_vector<int> nm_ta{num_tets_, -1};
-      thrust::device_vector<int> fl{assoc_capacity_, -1};
-      
-      int* num_redistributions;
-      cudaMalloc(&num_redistributions, sizeof(*num_redistributions));
-      cudaMemset(num_redistributions, 0, sizeof(*num_redistributions));
-      cudaDeviceSynchronize();
+      device_vector<int> nm{num_pts_, 1};
+      device_vector<int> nm_ta{num_tetra_, -1};
+      device_vector<int> fl{assoc_capacity, -1};
+      device_vector<int> num_redistributions{1, 0};
       
       std::cout << "Nominating and repairing points..." << std::endl;
       
       nominate<<<bpg, tpb>>>(
-        assoc_size_, ta_, pa_,
+        assoc_size,
+        ta.data().get(),
+        pa.data().get(),
         nm_ta.data().get(),
         nm.data().get());
       
+      cudaDeviceSynchronize();
+      
       thrust::fill(nm_ta.begin(), nm_ta.end(), -1);
       assert_unique_mesher<<<bpg, tpb>>>(
-        assoc_size_,
-        pa_,
+        assoc_size,
+        pa.data().get(),
         nm.data().get(),
-        ta_,
+        ta.data().get(),
         nm_ta.data().get());
       
       repair_nm_ta<<<bpg, tpb>>>(
-        assoc_size_, pa_, ta_,
+        assoc_size,
+        pa.data().get(),
+        ta.data().get(),
         nm.data().get(),
         nm_ta.data().get());
             
       assert_valid_nm_ta_mesher<<<bpg, tpb>>>(
-        assoc_size_,
+        assoc_size,
         nm_ta.data().get(),
-        pa_, ta_,
+        pa.data().get(),
+        ta.data().get(),
         nm.data().get());
       
       cudaDeviceSynchronize();
       
       std::cout << "Calculating fracture locations" << std::endl;
       
-      fract_locations(pa_, nm.data().get(), la_, assoc_size_, fl.data().get());
+      fract_locations(
+        pa.data().get(),
+        nm.data().get(),
+        la.data().get(),
+        assoc_size,
+        fl.data().get());
           
-      int const last_idx = assoc_size_ - 1;
-      int const num_new_tets = fl[last_idx] + (nm[thrust::device_ptr<int>{pa_}[last_idx]] * (std::bitset<32>{thrust::device_ptr<int>{la_}[last_idx]}.count() - 1));
+      int const last_idx = assoc_size - 1;
+      int const num_new_tets =
+        fl[last_idx] +
+        (nm[pa[last_idx]] *
+          (std::bitset<32>{la[last_idx]}.count() - 1));
           
       std::cout << "Fracturing and redistributing points" << std::endl;
           
       fracture<<<bpg, tpb>>>(
-        assoc_size_, num_tets_,
-        pa_, ta_, la_,
+        assoc_size, num_tetra_,
+        pa.data().get(),
+        ta.data().get(),
+        la.data().get(),
         nm.data().get(),
         fl.data().get(),
-        tets_);
+        tetra_);
       
       redistribute_pts<T><<<bpg, tpb>>>(
-        assoc_size_, num_tets_,
-        tets_,
-        pts_,
+        assoc_size, num_tetra_,
+        tetra_,
+        pts_.data().get(),
         nm.data().get(),
         nm_ta.data().get(),
         fl.data().get(),
-        pa_, ta_, la_,
-        num_redistributions);
+        pa.data().get(),
+        ta.data().get(),
+        la.data().get(),
+        num_redistributions.data().get());
       
       cudaDeviceSynchronize();
 
@@ -304,43 +303,53 @@ public:
 
       std::cout << "Stage 1 filtering..." << std::endl;
 
-      assoc_size_ = get_assoc_size(
-        pa_, ta_, la_,
-        assoc_capacity_);
+      assoc_size = get_assoc_size(
+        pa.data().get(),
+        ta.data().get(),
+        la.data().get(),
+        assoc_capacity);
       
       std::cout << "Stage 2 filtering..." << std::endl;
       
-      auto zip_begin = thrust::make_zip_iterator(
-        thrust::make_tuple(pa_, ta_, la_));
+      auto zip_begin = make_zip_iterator(
+        make_tuple(
+          pa.begin(),
+          ta.begin(),
+          la.begin()));
       
       auto const* raw_nm = nm.data().get();
       
-      assoc_size_ = thrust::distance(zip_begin, thrust::remove_if(
+      assoc_size = distance(zip_begin, remove_if(
         thrust::device,
-        zip_begin, zip_begin + assoc_size_,
-        [=] __device__ (thrust::tuple<int, int, int> const& t) -> bool
+        zip_begin, zip_begin + assoc_size,
+        [=] __device__ (tuple<int, int, int> const& t) -> bool
         {
-          return raw_nm[thrust::get<0>(t)] == 1;
+          return raw_nm[get<0>(t)] == 1;
         }));
       
       std::cout << "Filling..." << std::endl;
       
-      thrust::fill(
+      fill(
         thrust::device,
-        zip_begin + assoc_size_, zip_begin + assoc_capacity_,
-        thrust::make_tuple(-1, -1, -1));
+        zip_begin + assoc_size, zip_begin + assoc_capacity,
+        make_tuple(-1, -1, -1));
       
-      num_tets_ += num_new_tets;
+      num_tetra_ += num_new_tets;
       
-      std::cout << "number of tetrahedra: " << num_tets_ << std::endl;
-      std::cout << "size of associations: " << assoc_size_ << "\n" << std::endl;
+      std::cout << "number of tetrahedra: " << num_tetra_ << std::endl;
+      std::cout << "size of associations: " << assoc_size << "\n" << std::endl;
       
-      assert_mesher_associations<T><<<bpg, tpb>>>(pa_, ta_, la_, tets_, pts_, assoc_size_);
-      assert_positivity<T><<<bpg, tpb>>>(tets_, pts_, num_tets_);
+      assert_mesher_associations<T><<<bpg, tpb>>>(
+        pa.data().get(),
+        ta.data().get(),
+        la.data().get(),
+        tetra_,
+        pts_.data().get(),
+        assoc_size);
+      
+      assert_positivity<T><<<bpg, tpb>>>(tetra_, pts_.data().get(), num_tetra_);
       
       cudaDeviceSynchronize();
-      
-      cudaFree(num_redistributions);
     }
   }
 };
