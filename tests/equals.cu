3cc517289ec6c370495d2962d02f66b98b99ae07
Christian Mazakas <Christian Mazakas>
2016-11-20 16:13:53 -0800

    Fixed test to test things

--- tests/equals.cu
+++ tests/equals.cu
@@ -5,13 +5,11 @@ TEST(EqualsFunction, ShouldCompareAccurately)
 {
   EXPECT_EQ(true, eq(0.6f, 3.0f / 5));
   EXPECT_EQ(true, eq(0.1, 1.0 / 10));
-  
   EXPECT_EQ(false, eq(0.1, 1.1 / 10));
 }
 
 TEST(RoundToFunction, ShouldRoundNumbersAppropriately)
 {
-  EXPECT_EQ(0.333, round_to(1.0 / 3, 3));
-  
-  EXPECT_EQ(3.14285714, round_to(22.0 / 7, 8));
+  EXPECT_EQ(true, eq(0.333, round_to(1.0 / 3, 3)));
+  EXPECT_EQ(true, eq(3.14285714, round_to(22.0 / 7, 8)));
 }
