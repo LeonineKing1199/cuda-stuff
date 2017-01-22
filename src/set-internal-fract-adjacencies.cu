39584ed0443ec5cafb742cfe176810d0bb9acf0a
LeonineKing1199 <christian.mazakas@gmail.com>
2017-01-21 17:00:19 -0800

    Got internal adjacency routine working!

--- src/set-internal-fract-adjacencies.cu
+++ src/set-internal-fract-adjacencies.cu
@@ -13,14 +13,23 @@ auto set_interal_fract_adjacencies(
   };
 
   array<adjacency, 4> adj_relations;
-  for (
-    typename array<index_t, 4>::size_type i{}; i < fract_locs.size(); ++i)
+  array<uint8_t,   4> half_space_relations;
+
+  size_t const num_faces = 4;
+  for (size_t i = 0; i < num_faces; ++i) {
+    half_space_relations[i] = loc_code & (1 << i);
+  }
+
+  for (decltype(fract_locs.size()) i{}; i < fract_locs.size(); ++i)
   {
-    if (loc_code & (1 << i)) {
-      adj_relations[i] = { fract_locs[adjacency_map[i][0]],
-                           fract_locs[adjacency_map[i][1]],
-                           fract_locs[adjacency_map[i][2]],
-                           -1 };
+    if (half_space_relations[i]) {
+      adj_relations[i] = { 
+        half_space_relations[adjacency_map[i][0]] ? fract_locs[adjacency_map[i][0]] : -1,
+        half_space_relations[adjacency_map[i][1]] ? fract_locs[adjacency_map[i][1]] : -1,
+        half_space_relations[adjacency_map[i][2]] ? fract_locs[adjacency_map[i][2]] : -1,
+        -1 };
+    } else {
+      adj_relations[i] = { -1, -1, -1, -1 };
     }
   }
 
--- tests/set-internal-fract-adjacencies.cu
+++ tests/set-internal-fract-adjacencies.cu
@@ -6,24 +6,296 @@
 
 TEST_CASE("The internal adjacency setting")
 {
-	array<index_t, 4> const fract_locs = { 1000, 1001, 1002, 1003 };
+  array<index_t, 4> const fract_locs = { 1000, 1001, 1002, 1003 };
 
-	SECTION("1 to 4 fracture")
-	{
-		loc_t const loc_code = 15;
-		auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+  SECTION("1 to 4 fracture")
+  {
+    loc_t const loc_code      = 15;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
 
-		array<adjacency, 4> expected_adj_relations = {
-			adjacency{ 1003, 1002, 1001, -1 },
-			adjacency{ 1000, 1002, 1003, -1 },
-			adjacency{ 1000, 1003, 1001, -1 },
-			adjacency{ 1000, 1001, 1002, -1 }
-		};
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ 1003, 1002, 1001, -1 },
+      adjacency{ 1000, 1002, 1003, -1 },
+      adjacency{ 1000, 1003, 1001, -1 },
+      adjacency{ 1000, 1001, 1002, -1 }
+    };
 
-		for (
-			typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
-		{
-			REQUIRE(adj_relations[i] == expected_adj_relations[i]);
-		}
-	}
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  }
+
+  // Face tests
+
+  SECTION("1-to-3 flip (face 0)")
+  {
+    loc_t const loc_code      = 14;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ -1,   -1,   -1, -1 },
+      adjacency{ -1, 1002, 1003, -1 },
+      adjacency{ -1, 1003, 1001, -1 },
+      adjacency{ -1, 1001, 1002, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  }
+
+  SECTION("1-to-3 flip (face 1)")
+  {
+    loc_t const loc_code      = 13;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ 1003, 1002,   -1, -1 },
+      adjacency{ -1,     -1,   -1, -1 },
+      adjacency{ 1000, 1003,   -1, -1 },
+      adjacency{ 1000,   -1, 1002, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  }  
+
+  SECTION("1-to-3 flip (face 2)")
+  {
+    loc_t const loc_code      = 11;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ 1003,   -1, 1001, -1 },
+      adjacency{ 1000,   -1, 1003, -1 },
+      adjacency{   -1,   -1,   -1, -1 },
+      adjacency{ 1000, 1001,   -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  }  
+
+  SECTION("1-to-3 flip (face 3)")
+  {
+    loc_t const loc_code      = 7;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{   -1, 1002, 1001, -1 },
+      adjacency{ 1000, 1002,   -1, -1 },
+      adjacency{ 1000,   -1, 1001, -1 },
+      adjacency{   -1,   -1,   -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  }  
+
+  // Edge tests
+
+  SECTION("1-to-2 flip (edge 01)")
+  {
+    loc_t const loc_code      = 12;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ -1,   -1,   -1, -1 },
+      adjacency{ -1,   -1,   -1, -1 },
+      adjacency{ -1, 1003,   -1, -1 },
+      adjacency{ -1,   -1, 1002, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  } 
+
+  SECTION("1-to-2 flip (edge 02)")
+  {
+    loc_t const loc_code      = 10;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ -1,   -1,   -1, -1 },
+      adjacency{ -1,   -1, 1003, -1 },
+      adjacency{ -1,   -1,   -1, -1 },
+      adjacency{ -1, 1001,   -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  } 
+
+  SECTION("1-to-2 flip (edge 03)")
+  {
+    loc_t const loc_code      = 6;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ -1,   -1,   -1, -1 },
+      adjacency{ -1, 1002,   -1, -1 },
+      adjacency{ -1,   -1, 1001, -1 },
+      adjacency{ -1,   -1,   -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  } 
+
+  SECTION("1-to-2 flip (edge 12)")
+  {
+    loc_t const loc_code      = 9;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ 1003, -1, -1, -1 },
+      adjacency{   -1, -1, -1, -1 },
+      adjacency{   -1, -1, -1, -1 },
+      adjacency{ 1000, -1, -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  } 
+
+  SECTION("1-to-2 flip (edge 13)")
+  {
+    loc_t const loc_code      = 5;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{   -1, 1002, -1, -1 },
+      adjacency{   -1,   -1, -1, -1 },
+      adjacency{ 1000,   -1, -1, -1 },
+      adjacency{   -1,   -1, -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  } 
+
+  SECTION("1-to-2 flip (edge 23)")
+  {
+    loc_t const loc_code      = 3;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{   -1, -1, 1001, -1 },
+      adjacency{ 1000, -1,   -1, -1 },
+      adjacency{   -1, -1,   -1, -1 },
+      adjacency{   -1, -1,   -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  } 
+
+  // Vertex tests
+
+  SECTION("1-to-1 flip (vertx 0)")
+  {
+    loc_t const loc_code      = 1;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  }
+
+  SECTION("1-to-1 flip (vertx 1)")
+  {
+    loc_t const loc_code      = 2;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  }
+
+  SECTION("1-to-1 flip (vertx 2)")
+  {
+    loc_t const loc_code      = 4;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  }
+
+  SECTION("1-to-1 flip (vertx 3)")
+  {
+    loc_t const loc_code      = 8;
+    auto  const adj_relations = set_interal_fract_adjacencies(fract_locs, loc_code);
+ 
+    array<adjacency, 4> expected_adj_relations = {
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 },
+      adjacency{ -1, -1, -1, -1 }
+    };
+
+    for (
+      typename array<adjacency, 4>::size_type i{}; i < expected_adj_relations.size(); ++i)
+    {
+      REQUIRE(adj_relations[i] == expected_adj_relations[i]);
+    }
+  }
 }
